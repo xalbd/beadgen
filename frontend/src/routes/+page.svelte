75a991bbf393390765f2a9cdc99253325203e6bc
Albert Dong <albertd@ucla.edu>
2023-07-31 16:21:43 -0700

    toggle hides tip angle

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -12,11 +12,4 @@
 </section>
 
 <style>
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    flex: 0.6;
-  }
 </style>
--- frontend/src/routes/TipPreview.svelte
+++ frontend/src/routes/TipPreview.svelte
@@ -10,38 +10,32 @@
     shininess: 100,
   });
 
-  let loader = new STLLoader();
-  let scene = new THREE.Scene();
-  let mesh = new THREE.Mesh();
+  var loader = new STLLoader();
+  var scene = new THREE.Scene();
+  var mesh = new THREE.Mesh();
 
-  let tip_type: string = "Cone";
+  let tip_type: any;
   let radius = 2;
   let hole_radius = 0.5;
   let tip_angle = 90;
 
-  let api_path = `http://localhost:8000/api/tip?type=${
-    tip_type == "Cone" ? "0" : "1"
+  let api_path: string;
+  $: api_path = `http://localhost:8000/api/tip?type=${
+    !tip_type ? "0" : tip_type != "Cone"
   }&radius=${radius}&hole_radius=${hole_radius}&tip_angle=${tip_angle}`;
 
   onMount(() => {
     loadSTL(loader, mesh, scene, material, api_path);
     createViewer(scene);
-    console.log("done");
   });
 </script>
 
 <div class="flex flex-row w-screen">
   <div class="flex flex-col">
     <label for="tip-type"> Tip Type </label>
-    <select
-      name="tip-type"
-      bind:value={tip_type}
-      on:change={() => {
-        console.log(tip_type);
-      }}
-    >
-      <option> Cone </option>
-      <option> Sphere </option>
+    <select name="tip-type" bind:value={tip_type}>
+      <option value="Cone">Cone</option>
+      <option value="Sphere">Sphere</option>
     </select>
 
     <label for="radius-input"> Radius </label>
@@ -60,13 +54,24 @@
       bind:value={hole_radius}
     />
 
-    <label for="tip-angle-input"> Tip Angle </label>
-    <input
-      name="tip-angle-input"
-      class="h-10 bg-green-400"
-      type="number"
-      bind:value={tip_angle}
-    />
+    {#if !tip_type || tip_type == "Cone"}
+      <label for="tip-angle-input"> Tip Angle </label>
+      <input
+        name="tip-angle-input"
+        class="h-10 bg-green-400"
+        type="number"
+        bind:value={tip_angle}
+      />
+    {/if}
+
+    <button
+      class="h-10 rounded-full bg-green-500"
+      on:click={() => {
+        loadSTL(loader, mesh, scene, material, api_path);
+      }}
+    >
+      Generate STL
+    </button>
   </div>
   <div class="relative w-fill flex-1 min-w-0 bg-slate-500" id="model" />
 </div>
