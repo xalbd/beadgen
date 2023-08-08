35349ddde0e22ea73ba3d2cde7b1006cde4e13e4
Charles-BZ <zhang@Charles-XPS>
2023-08-08 14:26:25 -0700

    polygon generation (not finished)

--- frontend/src/routes/structures/+page.svelte
+++ frontend/src/routes/structures/+page.svelte
@@ -13,11 +13,12 @@
     let loader = new STLLoader();
     let scene = new THREE.Scene();
   
-    let struct_type = "triangle";
+    let struct_type = "polygon";
     let corner_type = "large-sphere";
     let side_length = 50;
     let beads_per_side = 5;
     let hole_radius = 1;
+    let num_sides = 5;
   
     let api_path: string;
     $: {
@@ -25,6 +26,8 @@
         api_path = `http://localhost:8000/api/square-struct?side_length=${side_length}&beads_per_side=${beads_per_side}&hole_radius=${hole_radius}`;
       } else if (struct_type == "triangle") {
         api_path = `http://localhost:8000/api/triangle-struct?side_length=${side_length}&beads_per_side=${beads_per_side}&hole_radius=${hole_radius}`;
+      } else if (struct_type == "polygon") {
+        api_path = `http://localhost:8000/api/polygon-struct?num_sides=${side_length}&side_length=${side_length}&beads_per_side=${beads_per_side}&hole_radius=${hole_radius}`;
       } 
     }
   
@@ -45,6 +48,7 @@
       <select name="result-type" bind:value={struct_type}>
         <option value="square">Square</option>
         <option value="triangle">Triangle</option>
+        <option value="polygon">Polygon</option>
       </select>
 
       <label for="corner-type"> Corner Type </label>
@@ -52,6 +56,16 @@
         <option value="large-sphere">Large Sphere</option>
       </select>
   
+      {#if !struct_type || struct_type == "polygon"}
+        <label for="num-sides-input"> Number of Sides </label>
+        <input
+          name="num-sides-input"
+          class="h-10 bg-purple-400"
+          type="number"
+          bind:value={num_sides}
+        />
+      {/if}
+
       <label for="side-length-input"> Side Length </label>
       <input
         name="side-length-input"
